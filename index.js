59e3289f6f5fd51a0cb07cfe0954f454d84aa58e
varshachavda <varsha@prepseed.com>
2024-07-22 22:46:20 +0530

    ejjs completed

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ const path = require('path')
 const userRoute = require("./routes/user_route");
 const staticRoute = require("./routes/static_route");
 const URL = require("./models/url_model");
-
-connectToMongoDB('mongodb://localhost:27017/short-url').then(() => console.log("mongodb connect"));
-app.use(express.json());
+const { url } = require('inspector');
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'));
+
+connectToMongoDB('mongodb://localhost:27017/short-url').then(() => console.log("mongodb connect"));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.get("/test",async(req,res)=>{
+    const allUrls = await URL.find({});
+    return res.render('home',{
+       urls:  allUrls
+    }); 
+})
+
 app.use('/url', urlRoute);
 app.use('/user', userRoute);
 app.use('/', staticRoute)
@@ -27,7 +37,6 @@ app.get('/:shortId', async (req, res) => {
             visitHistory: { timestamp: Date.now() }
         }
     });
-    // res.redirect(entry.redirectUrl);
 });
 
 app.listen(PORT, () => console.log('Server Started at PORT '))
--- routes/static_route.js
+++ routes/static_route.js
@@ -4,8 +4,20 @@ const router = require("./url_route");
 
 const app = express.Router();
 
+router.get('/',async (req,res)=>{
+    const allurls = await URL.find({})
+    return res.render('home', {
+        urls: allurls
+    });
+})
+
 router.get("/signup",(req,res) => {
     return res.render("signup")
 });
 
 module.exports = router ;
+
+/* <h1>Hello from Server</h1>
+<% urls.forEach(url => { %>
+ <li><%= url.shortId  %></li>
+<% }) %> */
